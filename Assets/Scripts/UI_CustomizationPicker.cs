729fd458de26dec4242f4b87512f30ef1cca5b77
AlexJeBoyy <a.strikwerda@sintlucasedu.nl>
2024-10-23 12:15:07 +0200

    Added better customization

---
+++ Assets/Scripts/UI_CustomizationPicker.cs
@@ -0,0 +1,45 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_CustomizationPicker : MonoBehaviour
+{
+    [SerializeField]
+    private CustomizableElement _customizableElement;
+
+    [SerializeField]
+    private Button _previousSpriteButton;
+
+    [SerializeField]
+    private Button _nextSpriteButton;
+
+    [SerializeField]
+    private Button _previousColorButton;
+
+    [SerializeField]
+    private Button _nextColorButton;
+
+    [SerializeField]
+    private TMP_Text _spriteID;
+
+    [SerializeField]
+    private Image _colorIcon;
+
+    private void Start()
+    {
+        _previousSpriteButton.onClick.AddListener(() =>
+            {
+                _customizableElement.PreviousSprite();
+                UpdateSpriteID();
+            });
+        _nextSpriteButton.onClick.AddListener(() =>
+        {
+            _customizableElement.NextSprite();
+            UpdateSpriteID();
+        });
+    }
+    private void UpdateSpriteID()
+    {
+        _spriteID.SetText(_customizableElement.SpriteIndex.ToString().PadLeft(2, '0'));
+    }
+}
