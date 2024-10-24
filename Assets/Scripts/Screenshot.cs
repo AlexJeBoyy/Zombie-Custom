a6cf4412c25819c6db123c16891438c88e8a0b87
AlexJeBoyy <a.strikwerda@sintlucasedu.nl>
2024-10-24 11:43:17 +0200

    Screenshots

--- Assets/Scripts/Screenshot.cs
+++ Assets/Scripts/Screenshot.cs
@@ -1,15 +1,48 @@
 using UnityEngine;
 using System;
 using UnityEditor;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 
 public class Screenshot : MonoBehaviour
 {
+    public GameObject UI;
+    private IEnumerator ScreenShot()
+    {
+        yield return new WaitForEndOfFrame();
+        Texture2D texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        
+        texture.ReadPixels(new Rect(0,0,Screen.width,Screen.height),0,0);
+        texture.Apply();
+
+        string name = "Screenshot-" + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".png";
+        
+        string path = Path.Combine(Application.dataPath, "Screenshots");
+
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+        
+        //pc
+        byte[] bytes = texture.EncodeToPNG();
+        //File.WriteAllBytes(Path.Combine(path, name), bytes);
+        //File.WriteAllBytes(Application.dataPath + path + name, bytes);
+        File.WriteAllBytes(Path.Combine(path, name), bytes);
+        Destroy(texture);
+        Vector2 endPos = new Vector2(0,0);
+        UI.transform.position = endPos;
+
+        Debug.Log("Screenshot saved to: " + Path.Combine(path, name));
+    }
     
     public void TakeScreen()
     {
-        ScreenCapture.CaptureScreenshot(Application.dataPath + "/Screenshots/" + "Screenshot-" + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss")+".png", 2);
-        //AssetDatabase.Refresh();
+        
+        Vector2 startPos = new Vector2(10, 10);
+        UI.transform.position = startPos;
+        StartCoroutine("ScreenShot");
+        
     }
 }
