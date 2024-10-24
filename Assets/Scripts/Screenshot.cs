40a759835e07df47428e48ccbfc2265084d97d2e
AlexJeBoyy <alexstrikwerda@hotmail.com>
2024-10-24 02:04:52 +0200

    Backgrounds

--- Assets/Scripts/Screenshot.cs
+++ Assets/Scripts/Screenshot.cs
@@ -10,6 +10,6 @@ public class Screenshot : MonoBehaviour
     public void TakeScreen()
     {
         ScreenCapture.CaptureScreenshot(Application.dataPath + "/Screenshots/" + "Screenshot-" + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss")+".png", 2);
-        //AssetDatabase.Refresh();/
+        //AssetDatabase.Refresh();
     }
 }
